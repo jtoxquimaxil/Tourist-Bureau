b9e8b488c96df8140a86fb9d4701f38f4e71a1bb
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-11-21 11:56:26 -0500

    add two arrays to my JS files

---
+++ scripts/activities.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let activities = [
+    {
+        category: "Adventures",
+        id: "A101",
+        name: "Hot Air Balloon Ride",
+        description: "Enjoy a lovely hot air balloon ride over the valley at sunrise. Call 800-555-1212 to reserve a date/time after you complete your e-ticket purchase.",
+        location: "121 S. Main Street",
+        price: 365.00
+    }
+    {
+
+        category: "Outdoor Adventure",
+        id: "A102",
+        name: "Hiking Expedition",
+        description: "Explore scenic trails and enjoy the beauty of nature on this guided hiking adventure.",
+        location: "Mountain National Park",
+        price: 30.99
+    },
+    {
+
+        category: "Cultural Experience",
+        id: "A103",
+        name: "Museum Tour",
+        description: "Immerse yourself in the rich history and artistry of the region with a curated museum tour.",
+        location: "City Art Museum",
+        price: 20.50
+    },
+    {
+
+        category: "Culinary Delight",
+        id: "A104",
+        name: "Cooking Class",
+        description: "Learn the art of cooking from expert chefs and savor the flavors of a hands-on culinary experience.",
+        location: "Downtown Culinary School",
+        price: 50.75
+    },
+    {
+
+        category: "Relaxation Retreat",
+        id: "A105",
+        name: "Spa Day",
+        description: "Indulge in a day of relaxation and rejuvenation with spa treatments and wellness activities.",
+        location: "Tranquil Spa Resort",
+        price: 80.00
+    }
+];
---
+++ scripts/hikingPage.js
@@ -0,0 +1,65 @@
+"use strict";
+
+let hikes = [
+    {
+      id: "H101",
+      category: "Outdoor Adventure",
+      name: "Some Hike Name",
+      description: "Some description of the hike.",
+      scenicImage: "hike_1.png",
+      trailMapImage: "hike_2.png",
+      length: "2 miles",
+      difficulty: 2,
+      location: "Mountain National Park",
+      price: 30.99
+    },
+    {
+      id: "H102",
+      category: "Outdoor Adventure",
+      name: "Mountain Summit Trek",
+      description: "Challenge yourself with a breathtaking trek to the summit of the highest peak in the region.",
+      scenicImage: "hike_3.png",
+      trailMapImage: "hike_4.png",
+      length: "5 miles",
+      difficulty: 4,
+      location: "Highland Mountains",
+      price: 45.50
+    },
+    {
+      id: "H103",
+      category: "Nature Exploration",
+      name: "Waterfall Discovery Trail",
+      description: "Embark on a serene journey through lush forests to discover hidden waterfalls along the way.",
+      scenicImage: "hike_5.png",
+      trailMapImage: "hike_6.png",
+      length: "3 miles",
+      difficulty: 3,
+      location: "Forest Valley",
+      price: 25.75
+    },
+    {
+      id: "H104",
+      category: "Scenic Beauty",
+      name: "Sunset Ridge Walk",
+      description: "Enjoy the magic of a sunset as you stroll along a ridge offering panoramic views of the surrounding landscape.",
+      scenicImage: "hike_7.png",
+      trailMapImage: "hike_8.png",
+      length: "2.5 miles",
+      difficulty: 2,
+      location: "Sunset Ridge Park",
+      price: 20.00
+    },
+    {
+      id: "H105",
+      category: "Family-Friendly",
+      name: "Nature Discovery Trail",
+      description: "Perfect for families, this trail features interactive points for kids to learn about local flora and fauna.",
+      scenicImage: "hike_9.png",
+      trailMapImage: "hike_10.png",
+      length: "1.5 miles",
+      difficulty: 1,
+      location: "Family Nature Preserve",
+      price: 15.00
+    }
+  ];
+  
